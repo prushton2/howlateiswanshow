044d64cc2d4e6e91b8e38f353c5e443aefd6e946
prushton2 <peterrushton418@gmail.com>
2022-07-18 08:32:40 -0400

    added a time check for when wan show should be live

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ const express = require("express")
 const fs = require("fs")
 
 let indexfile = 'src/html/index.html'
+let wanShowTimes = {
+    start: {
+        day: 6,
+        hour: 0
+    },
+    end: {
+        day: 6,
+        hour: 2
+    }
+}
+
+// wanShowTimes = {start: {day: 0,hour: 0},end: {day: 6,hour: 23}}
+
+
 
 const app = express()
 
@@ -12,7 +26,24 @@ app.get("/", async(req, res) => {
     res.end(index)
 })
 
-app.get("/timeLate")
+app.get("/status", async(req, res) => {
+    const date = new Date()
+    const dateJson = {"day": date.getUTCDay(), "hour": date.getUTCHours()}
+
+    //check if stream is live
+
+
+    //check if its time for wan show
+    if(dateJson["day"] >= wanShowTimes["start"]["day"] && dateJson["hour"] >= wanShowTimes["start"]["hour"]) {
+        if(dateJson["day"] <= wanShowTimes["end"]["day"] && dateJson["hour"] <= wanShowTimes["end"]["hour"]) {
+            res.send("late")
+            return
+        }
+    }
+
+    //else say its not time yet
+    res.send("not on yet")
+})
 
 
 app.listen(8080, () => {})
